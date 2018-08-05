aaa6eb636bfcc903068f1dac74542f02a26fd66c
Stephanie Wang <stephaniewang@Stephanies-MBP.wv.cc.cmu.edu>
2018-08-05 19:04:46 -0400

    Finished 2c.

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -9,3 +9,17 @@ model_select <- function(covariates, responses, cutoff) {
   result=(summary(reg)$coefficients[which(summary(reg)$coefficients[c("covariates","responses"),4]) <= cutoff])
   result
 }
+
+run_simulation <- function(n_trials, n, p, cutoff) {
+  for (i in 1:3) {
+    v=vector()
+    n = n[i]
+    p = p[i]
+    for (j in 1:n_trials) {
+      data = generate_data(n,p)
+      vals=(model_select(data[[1]],data[[2]],cutoff))
+      v=c(v, vals)
+    }
+    hist(v)
+  }
+}
